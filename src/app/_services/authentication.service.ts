a1cf1c6e943c9191065fea647ce376f1b20908db
Jeff Andersen <jaandersen@ucdavis.edu>
2018-07-30 22:53:59 -0700

    Updates to call token service.

--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -7,8 +7,10 @@ export class AuthenticationService {
     constructor(private http: HttpClient) { }
 
     login(username: string, password: string) {
-        return this.http.post<any>('/api/authenticate', { username: username, password: password })
-            .pipe(map((res:any) => {
+        // return this.http.post<any>('/api/authenticate', { username: username, password: password })
+        // return this.http.post<any>('https://public-api.wordpress.com/rest/v1.1/sites/vocon-it.com/posts', { username: username, password: password })
+        return this.http.post<any>('http://localhost:8080/token/generate-token', { username: username, password: password })
+            .pipe(map((res: any) => {
                 // login successful if there's a jwt token in the response
                 if (res && res.token) {
                     // store username and jwt token in local storage to keep user logged in between page refreshes
--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -8,6 +8,6 @@ export class UserService {
     constructor(private http: HttpClient) { }
 
     getAll() {
-        return this.http.get<User[]>('/api/users');
+        return this.http.get<User[]>('http://localhost:8080/user');
     }
 }
